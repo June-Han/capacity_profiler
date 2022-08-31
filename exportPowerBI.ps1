8f8b3a4f5a70d4a70ca00ab0439829571623880c
June-Han <julialiu08@gmail.com>
2022-08-31 20:00:48 +0800

    Squash some bugs for dataset export

--- exportPowerBI.ps1
+++ exportPowerBI.ps1
@@ -2,8 +2,9 @@ $ErrorActionPreference = “Stop”
 
 $dataPath = "$home/quotautil"
 
-Install-Module -Name MicrosoftPowerBIMgmt
+Install-Module -Name MicrosoftPowerBIMgmt -Force
 
+Write-Host 'Logging into PowerBI Service Account...' -ForegroundColor Green
 Connect-PowerBIServiceAccount
 
 $WSName = "SubsOverview"
@@ -41,33 +42,36 @@ function DataSetCheck() {
 # Creating/Retrieving workspace in PowerBI
 if (WorkSpaceCheck)
 {
-    Write-Host "`nIntended Workspace found. Retrieving`n"  
+    Write-Host "`nIntended Workspace found. Retrieving...`n"  -ForegroundColor Green
     $workspace = Get-PowerBIWorkspace -Name $WSName
+    $workspace
 }
 else
 {
-    Write-Host "`nIntended Workspace not found. Creating...`n"  
+    Write-Host "`nIntended Workspace not found. Creating...`n"  -ForegroundColor Red
     $workspace = New-PowerBIWorkspace -Name $WSName #Uses shared capacity
+    $workspace
 }
 
 # Creating/Retrieving dataset in PowerBI
 if (DataSetCheck)
 {
-    Write-Host "`nIntended Workspace found. Retrieving`n"
+    Write-Host "`nIntended Dataset found. Retrieving`n" -ForegroundColor Green
     $wsdataset = Get-PowerBIDataset -Name $DSName -WorkspaceId $workspace.Id
+    $wsdataset
 }
 else 
 {
-    Write-Host "`nIntended Workspace not found. Creating new dataset`n"
+    Write-Host "`nIntended Dataset not found. Creating new dataset`n" -ForegroundColor Red
     
     #Creating PowerBI Table 
-    $col1 = New-PowerBIColumn -Name "datetime_in_utc" -DataType DateTime
+    $col1 = New-PowerBIColumn -Name "datetime_in_utc" -DataType String
     $col2 = New-PowerBIColumn -Name "subscription_name" -DataType String
     $col3 = New-PowerBIColumn -Name "resource_name" -DataType String
     $col4 = New-PowerBIColumn -Name "location" -DataType String
-    $col5 = New-PowerBIColumn -Name "current_value" -DataType Int64
-    $col6 = New-PowerBIColumn -Name "limit" -DataType Int64
-    $col7 = New-PowerBIColumn -Name "limit" -DataType Int64
+    $col5 = New-PowerBIColumn -Name "current_value" -DataType String
+    $col6 = New-PowerBIColumn -Name "limit" -DataType String
+    $col7 = New-PowerBIColumn -Name "usage" -DataType String
     $table = New-PowerBITable -Name $tblName -Columns $col1, $col2, $col3, $col4, $col5, $col6, $col7
 
     # Create Dataset (Subscription_Data)
@@ -76,7 +80,7 @@ else
     # Adding dataset to workspace
     $wsdataset = Add-PowerBIDataset -Dataset $dataset -WorkspaceId $workspace.Id
 
-    Write-Host "`nCreated Workspace Dataset`n"
+    Write-Host "`nCreated Workspace Dataset`n" -ForegroundColor Green
     $wsdataset
 }
 
@@ -86,8 +90,9 @@ Get-ChildItem $dataPath -Filter "*.csv" |ForEach-Object {
     $file=$_              
     
     #Import csv and add column with filename
-    #$data = Import-Csv $file.FullName | Select-Object @{Label="File";Expression={$file.Name}}, *
+    Write-Host "`nPopulating table in dataset...`n" -ForegroundColor Green
 
     # Send data to PowerBI
     Add-PowerBIRow -DatasetId $wsdataset.Id -TableName $tblName -Rows (Import-Csv $file.FullName)
+    Write-Host "`n===== Dataset Export to PowerBI is a success =====`n" -ForegroundColor Green
 }
--- test.ps1
+++ test.ps1
@@ -1,14 +1,14 @@
 Clear-Host
 
-$ErrorActionPreference = “Stop”
+$ErrorActionPreference = "Stop"
 
 $currentPath = (Split-Path $MyInvocation.MyCommand.Definition -Parent)
 
  
 #Create Archive Folder 
-#new-item -Name “Archive” -Force -ItemType directory -Path “$currentPath\CSVData”  | Out-Null
+#new-item -Name "Archive" -Force -ItemType directory -Path "$currentPath\CSVData"  | Out-Null
 
-# Import-Module “$currentPath\Modules\PowerBIPS” -Force 
+# Import-Module "$currentPath\Modules\PowerBIPS" -Force 
 Install-Module PowerBIPS
 Import-Module -Name PowerBIPS
 
@@ -17,25 +17,25 @@ while($true)
 
     # Iterate each CSV file and send to PowerBI
 
-    Get-ChildItem “$currentPath\CSVData” -Filter “*.csv” |ForEach-Object { 
+    Get-ChildItem "$currentPath\CSVData" -Filter "*.csv" |ForEach-Object { 
 
         $file=$_               
 
         #Import csv and add column with filename
 
-        $data = Import-Csv $file.FullName | Select-Object @{Label=”File”;Expression={$file.Name}}, *
+        $data = Import-Csv $file.FullName | Select-Object @{Label="File";Expression={$file.Name}}, *
 
         # Send data to PowerBI
 
-        $data |  Out-PowerBI -dataSetName “CSVSales” -tableName “Sales” -types @{“Sales.OrderDate”=”datetime”; “Sales.SalesAmount”=”double”; “Sales.Freight”=”double”} -batchSize 300 -verbose
+        $data |  Out-PowerBI -dataSetName "CSVSales" -tableName "Sales" -types @{"Sales.OrderDate"="datetime"; "Sales.SalesAmount"="double"; "Sales.Freight"="double"} -batchSize 300 -verbose
 
         # Archive the file
 
-        Move-Item $file.FullName “$currentPath\CSVData\Archive\” -Force
+        Move-Item $file.FullName "$currentPath\CSVData\Archive\" -Force
 
     }
 
-    Write-Output “Sleeping…”
+    Write-Output "Sleeping…"
 
     Start-Sleep -Seconds 5
 
